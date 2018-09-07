4ddd0031d6cfbc8f813fc6fc80bbcafe5d443f3e
rosatamarindus <rosatamarindus@gmail.com>
2018-09-07 16:01:52 +0700

    Add file Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,27 @@
+public class Dosen {
+
+    private String namaDosen;
+    private String nip;
+
+    public Dosen(String namaDosen, String nip) {
+        this.namaDosen = namaDosen;
+        this.nip = nip;
+    }
+
+    public String getNamaDosen() {
+        return namaDosen;
+    }
+
+    public void setNamaDosen(String namaDosen) {
+        this.namaDosen = namaDosen;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+}
+
